faf0a6a127c30fd70ef6891e90e1b986a36354c0
MohamedBenAhmed55 <mohamed.benahmed@etudiant-fsegt.utm.tn>
2024-11-04 20:01:35 +0100

    Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.myObserver.subscribe((val) => {
+      console.log(val);
+    });
+  }
+
   title = 'bootstrap-angular';
+  myObserver = new Observable((observer) => {
+    console.log('Observer Starts');
+    setTimeout(() => {
+      observer.next('1');
+    }, 1000);
+    setTimeout(() => {
+      observer.next('2');
+    }, 1000);
+    setTimeout(() => {
+      observer.next('3');
+    }, 1000);
+    setTimeout(() => {
+      observer.next('4');
+    }, 1000);
+    setTimeout(() => {
+      observer.next('5');
+    }, 1000);
+  });
 }
