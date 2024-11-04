37641e714172b39a8cdab7c7a40b4fea3ef5d68c
MohamedBenAhmed55 <mohamed.benahmed@etudiant-fsegt.utm.tn>
2024-11-04 22:18:40 +0100

    Subject Baheviour

--- src/app/sub1/sub1.component.ts
+++ src/app/sub1/sub1.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sub1',
   templateUrl: './sub1.component.html',
-  styleUrl: './sub1.component.css'
+  styleUrl: './sub1.component.css',
 })
 export class Sub1Component {
-  constructor(){}
-  onChange(userid:any){
-    console.log(userid.value)
+  constructor() {}
+  onChange(userid: any) {
+    console.log(userid.value);
   }
 }
